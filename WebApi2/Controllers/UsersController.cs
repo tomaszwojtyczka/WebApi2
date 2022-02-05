592cc937cc74bf8b9025a6449ab926da693f2cbc
Tomasz Wojtyczka /desktop <wojtyczka.t@gmail.com>
2022-02-05 23:55:01 +0100

    Add project files.

---
+++ WebApi2/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApi2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = "select Id, UserName, UserStatus from dbo.users";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CrmConnectionString");
+            SqlDataReader reader;
+            using (SqlConnection conn = new SqlConnection(sqlDataSource))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    reader = cmd.ExecuteReader();
+                    table.Load(reader);
+                    reader.Close();
+                    conn.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+    }
+}
